8bb5b98a4e9e35b4e020b3930f08226b71074913
POTATOPROGAMMER <codercode1010@gmail.com>
2024-12-07 11:46:13 +0400

    commit1

---
+++ activity1.py
@@ -0,0 +1,18 @@
+age=int(input())
+if age >10 :
+    print("rons age is above 10")
+    if age <20 :
+        print("rons age is below 20")
+    else:
+        print("ron is above 20")
+else:
+    print("rons age is below 10")        
+
+
+
+ 
+
+
+
+
+
